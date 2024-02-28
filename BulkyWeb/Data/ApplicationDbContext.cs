ce3ec3947c3fde9168aebc28d7c81eabe727080e
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-02-28 19:21:06 +0200

    add category model and seeding the category table

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasData(
+                new Category { Name = "Action" , DisplayOrder = 1, Id = 1 },
+                new Category { Name = "Scifi"  , DisplayOrder = 2, Id = 2 },
+                new Category { Name = "History", DisplayOrder = 3, Id = 3 }
+            );
+    }
+
+    public  virtual DbSet<Category> Categories { get; set; }
+}
