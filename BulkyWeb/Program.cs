5f9b5b913295ba0efafd506ce2f91a234a2feb76
abdullrhman <3brhmaaan@gmail.com>
2025-02-18 14:33:27 +0200

    fixing bug related to IEmailSender interface using AddDefaultIdentity adds some fake implementation for IEmailSender , but when changing it to AddIdentity Method to configure roles manage we have to provide the implementation , so here i providing an implementation for this interface

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -3,6 +3,8 @@ using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Bulky.Utility;
 
 namespace BulkyWeb
 {
@@ -19,10 +21,11 @@ namespace BulkyWeb
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services
-                .AddDefaultIdentity<IdentityUser>()
+                .AddIdentity<IdentityUser , IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 			builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+			builder.Services.AddScoped<IEmailSender , EmailSender>();
             builder.Services.AddRazorPages();
 
 			var app = builder.Build();
